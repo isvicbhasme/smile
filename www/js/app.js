335e254e929bea7e3339fd99d1f66d2c082bdeee
Isaac Bhasme <isvicbhasme@live.com>
2015-10-11 23:32:09 +0530

    html registration form validation

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 
-angular.module('app', ['ionic', 'app.controllers'])
+angular.module('app', ['ionic', 'app.controllers', 'ngMessages'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
